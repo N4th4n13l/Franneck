653155ffae1692f4c9fc6c45cc203a71c749b11b
N4th4n13l <lobsiger@gus.ch>
2022-09-25 15:52:02 +0200

    25092022_2

--- 22.Matpltlib.py
+++ 22.Matpltlib.py
@@ -4,7 +4,7 @@
 import matplotlib.pyplot as plt
 
 # -----------------------------
-data = [1, 3, 5, 7]
+data = [1, 3, 5, 2]
 
 # Schritt 1 welche daten werden geplottet
 plt.plot(data, c='r')
@@ -14,5 +14,12 @@ plt.scatter(range(len(data)), data, c='b') # <<<<<<<<<<<<<<<< Weitere Möglichke
 plt.xlabel('X Werte')
 plt.ylabel('Y Werte')
 # Ausgabe als verbundene Linien
+# z.b. Mittellinie als Schwellwert in gelb
+x1 = 0
+x2 = 5
+y1 = 3
+y2 = 3
+plt.plot((x1, x2), (y1, y2), c='y')
+
 plt.show()
 plt.savefig('Dateien/01.png')
---
+++ 23.Numpy.py
@@ -0,0 +1,14 @@
+# Numpy |
+#
+import numpy as np
+
+data = [1.0, 2.0, 5.0, 8.0]
+print(data)
+
+data2 = np.asarray(data, dtype=np.float64)
+print(data2)
+
+data3 = np.array([1.0, 2.0, 5.0, 8.0], dtype=np.float64)
+print(data3)
+
+
