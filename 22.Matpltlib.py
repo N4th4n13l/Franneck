5a243c69f938645f90b6a45fc9899a4907367164
N4th4n13l <lobsiger@gus.ch>
2022-09-25 15:22:33 +0200

    25092022_2

--- 22.Matpltlib.py
+++ 22.Matpltlib.py
@@ -1,19 +1,18 @@
 # Matplotlib |
 # Daten grafisch darstellen
-import random
+
 import matplotlib.pyplot as plt
 
 # -----------------------------
-#data = [1, 3, 5, 7]
-data = random.randint(1,10)in range (10)
-for i in data:
-      print(i)
+data = [1, 3, 5, 7]
+
 # Schritt 1 welche daten werden geplottet
 plt.plot(data, c='r')
-#Weitere Mögliche Plot Befehle - Nur Punkte Anzeigen
-#plt.scatter(range(len(data)), data, c='b') # <<<<<<<<<<<<<<<< Weitere Möglichkeit
+# Weitere Mögliche Plot Befehle - Nur Punkte Anzeigen
+plt.scatter(range(len(data)), data, c='b') # <<<<<<<<<<<<<<<< Weitere Möglichkeit
 # Anzeige als x und y Koordinaten
 plt.xlabel('X Werte')
 plt.ylabel('Y Werte')
 # Ausgabe als verbundene Linien
 plt.show()
+plt.savefig('Dateien/01.png')
