202e5ae69077d0b466271403033e91b33003853c
N4th4n13l <lobsiger@gus.ch>
2022-09-26 18:58:23 +0200

    25092022_2

--- 17.Module.py
+++ 17.Module.py
@@ -1,4 +1,5 @@
 # Module | laden und eigene erstellen
+# __init__.py
 
 import numpy # Befehl zum importieren
 import numpy as np # Modul importieren und mit anderem Namen weiterverwenden.
@@ -12,3 +13,13 @@ from numpy import mean # alles reinladen von dem Modul - Nicht unbedingt benutze
 print(mean([1,2]))
 
 EigenesModul.func01()
+
+# ------------------ Eine Software besteht aus diversen Modulen! Eigenen oder externen -------------------
+# Wir brauchen einen Einstiegspunkt
+# from MODUL import FUNKTION - so können wir gezielt auswählen was wir wollen
+
+# wenn wir einen Unterordner haben braucht es eine leere python Datei mit dem Code >>>>>>>>>>>>>>>>
+# __init__.py   <<<<<<<<< nur wenn wir Klassen und Funktionen nutzen wollen
+
+# dann muss es heissen >>> from ORDNER import Modul und aufgerufen wir es mit MODUL.FUNKTION
+# oder from ORDNER.MODUL import FUNKTION
--- 20.Objekte.py
+++ 20.Objekte.py
@@ -6,3 +6,47 @@ items.append(3)
 items.pop(-1)
 print(id(items)) # wo ist das Object gespeichert?
 
+# --------------------------------------------------
+# das ist auch ein Objekt
+
+class Dog:
+    def bark(self): # in einer Klasse definieren wir eine Funktion mit self - Objektorientierte Programmierung
+        print('wuff wuff')
+
+
+roger = Dog()
+print(type(roger)) # = <class '__main__.Dog'> roger bezieht sich auf die Klasse Dog
+
+# ------------------ mit inet Constructor -----------------------------
+class Dog:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def bark(self):
+        print('Woof')
+
+
+roger = Dog('Roger', 8)
+print(roger.name)
+print(roger.age)
+(roger.bark())  # wir geben nur die Funktion aus - print ist ja schon in der Funkion bark entalten
+
+# ----------------------------------------- JETZT KOMMT ES ------------------------------------
+
+class Animal:
+    def walk(self):
+        print('Walking')
+
+class Dog(Animal):   # !!!! @@ Wenn wir uns hier auf die Klasse Animal beziehen
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def bark(self):
+        print('Woof')
+
+
+roger = Dog('Roger', 8)
+print(roger.name)
+print(roger.age)
+roger.bark()  # wir geben nur die Funktion aus - print ist ja schon in der Funktion bark oben enthalten
+roger.walk() # @@ Können wir uns hier auch auf die Klasse Animal beziehen
